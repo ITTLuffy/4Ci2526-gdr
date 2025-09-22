2b3a8a404adc570d04791360b991801da25ac35b
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-09-22 20:39:42 +0200

    Fix player attributes and enhance battle logic; add ranged attack for Legolas and display damage dealt

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
+        
         Squadra team = new Squadra();
 
-        Giocatore aragorn = new Guerriero("Aragorn", 100, 0, 90, Razza.Umano);
-        Giocatore gandalf = new Mago("Gandalf", 70, 100, 20, Razza.Transformer);
+        Giocatore aragorn = new Guerriero("Aragorn", 100, 0, 80, Razza.Umano);
+        Giocatore gandalf = new Mago("Gandalf", 70, 100, 20, Razza.Ent);
         Giocatore legolas = new Arciere("Legolas", 90, 0, 50, Razza.Elfo);
 
         team.aggiungiGiocatore(aragorn);
@@ -13,20 +13,25 @@ public class App {
 
         team.salutaTutti();
 
+        System.out.println("Battaglia");
         Equip spada = new Equip("Anduril", 20, TipoEquip.ArmaMischia);
         aragorn.aggiungiEquip(spada);
-        System.out.println("Peso inventario: " + aragorn.getPeso());
-        
+        System.out.println("Peso inventario: " + aragorn.getPeso()); 
 
         // TODO
         Equip elmo = new Equip("Elmo", 5, TipoEquip.Armatura);
         Equip armor = new Equip("Armatura", 30, TipoEquip.Armatura);
-        
+        Equip arco = new Equip("Arco", 5, TipoEquip.ArmaRanged);
         legolas.aggiungiEquip(armor);
         legolas.aggiungiEquip(elmo);
+        legolas.aggiungiEquip(arco);
 
+        int danno = aragorn.attaccaMischia(legolas, 50);
+        System.out.printf("Inflitti %d danni\n", danno);
+        team.salutaTutti();
 
-        aragorn.attaccaMischia(legolas, 90);
+        int danno2 = legolas.attaccaRanged(aragorn, 50);
+        System.out.printf("Inflitti %d danni\n", danno2);
         team.salutaTutti();
 
 
